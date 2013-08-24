821648c8a0be5bc66e89876ad9078d7b84723a96
lamas14 <lamas14@mycu.concord.edu>
2013-08-24 18:47:45 -0400

    Robot game alpha v

---
+++ src/com/meroapps/robotgame/SplashLoadingScreen.java
@@ -0,0 +1,47 @@
+package com.meroapps.robotgame;
+
+import com.meroapps.framework.Game;
+import com.meroapps.framework.Graphics;
+import com.meroapps.framework.Screen;
+import com.meroapps.framework.Graphics.ImageFormat;
+
+public class SplashLoadingScreen extends Screen {
+    public SplashLoadingScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        Graphics g = game.getGraphics();
+        Assets.splash= g.newImage("splash.jpg", ImageFormat.RGB565);
+
+        
+        game.setScreen(new LoadingScreen(game));
+
+    }
+
+    @Override
+    public void paint(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void backButton() {
+
+    }
+}
